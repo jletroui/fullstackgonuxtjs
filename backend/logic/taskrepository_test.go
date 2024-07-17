c2b546fc7d5fb3befde2b90f6ebb06c495ce72b1
Julien Letrouit <julien.letrouit@gmail.com>
2024-07-17 17:10:38 -0400

    Adds a more thorough test in the data access layer

--- backend/logic/taskrepository_test.go
+++ backend/logic/taskrepository_test.go
@@ -20,8 +20,24 @@ func (suite *PostgresTaskRepositoryTestSuite) SetupSuite() {
 	suite.sut = logic.NewPostgresTaskRepository(Db)
 }
 
+func (suite *PostgresTaskRepositoryTestSuite) SetupTest() {
+	_, err := Db.Exec("TRUNCATE tasks")
+	suite.NoError(err)
+}
+
 func (suite *PostgresTaskRepositoryTestSuite) TestWhenNoTaskThenCountReturn0() {
 	count, err := suite.sut.Count()
 	suite.NoError(err)
 	suite.Equal(0, count)
+}
+
+func (suite *PostgresTaskRepositoryTestSuite) TestWhen2TasksThenCountReturn2() {
+	err := suite.sut.CreateTask("abc")
+	suite.NoError(err)
+	err = suite.sut.CreateTask("def")
+	suite.NoError(err)
+
+	count, err := suite.sut.Count()
+	suite.NoError(err)
+	suite.Equal(2, count)
 }
